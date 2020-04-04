51ffdf91df01ee48f110882eecbf9f4c96a41e51
Ivan Ereshchenko <ivan.ereshchenko18@imperial.ac.uk>
2020-04-04 11:41:19 +0100

    changed "pong" to "Pong!"

--- commands/ping.js
+++ commands/ping.js
@@ -5,6 +5,6 @@ const settings = require("../settings.json");
 exports.run = (client, message, args, config) => {
     return new Promise((resolve, reject) => {
         let channel = message.channel;
-        channel.send("pong");
+        channel.send("Pong!");
     });    
 }
