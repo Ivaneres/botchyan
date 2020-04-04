f3efba339826be4cf3b3e4caf69b8c18942a0190
Ivan Ereshchenko <ivan.ereshchenko18@imperial.ac.uk>
2020-04-04 02:10:23 +0100

    Changed examplereply

--- commands/examplereply.js
+++ commands/examplereply.js
@@ -5,16 +5,6 @@ const fs = require("fs");
 
 exports.run = (client, message, args, config) => {
     return new Promise(async (resolve, reject) => {
-        switch(args[0]){
-            case "!Yuni":
-                message.channel.send("Yuni nanodesu!");
-                break;
-            case "!ICAS":
-                message.channel.send("Imperial cares deeply about our staff and students");
-                break;
-            case "!KCL":
-                message.channel.send("[insert placeholder]");
-                break;
-        }
+        message.channel.send(`Example response!`);
     });
 }
